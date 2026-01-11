7bfd462ed270e36ca693ca44828efa1a88bbe8c8
JuhMaran <julianemaran@gmail.com>
2026-01-11 18:37:52 -0300

    db: adiciona scripts para migrations com flyway

---
+++ user-service/src/main/resources/db/migration/V2__insert_users.sql
@@ -0,0 +1,4 @@
+insert into users (username, first_name, last_name, email, phone_number, user_status)
+values ('juliane', 'Juliane', 'Maran', 'juliane@petstore.com', '11999999999', 1),
+       ('admin', 'Admin', 'System', 'admin@petstore.com', '11000000000', 1),
+       ('user_test', 'User', 'Test', 'user@test.com', '11888888888', 0);
