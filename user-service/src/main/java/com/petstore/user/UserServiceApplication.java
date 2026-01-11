56c1c8105f7a4c9b460da5bd67714950d33c3901
JuhMaran <julianemaran@gmail.com>
2026-01-11 15:43:04 -0300

    build: adiciona h2, validation, jpa e processor | fix: ajusta o fluxo GET /users/{username}

--- user-service/src/main/java/com/petstore/user/UserServiceApplication.java
+++ user-service/src/main/java/com/petstore/user/UserServiceApplication.java
@@ -2,14 +2,12 @@ package com.petstore.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
-@EnableElasticsearchRepositories(basePackages = "com.petstore.user.repository")
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UserServiceApplication.class, args);
+  }
 
 }
